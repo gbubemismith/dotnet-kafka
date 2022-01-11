5fe933e67663c3088cdc5c4ca16815df1b2c37c0
gbubemismith <gsmithwalter@gmail.com>
2022-01-11 10:29:05 +0100

    made changes

--- dotnet-kafka-console/Program.cs
+++ dotnet-kafka-console/Program.cs
@@ -1,4 +1,9 @@
 ﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Confluent.Kafka;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
 namespace dotnet_kafka_console
 {
@@ -7,6 +12,41 @@ namespace dotnet_kafka_console
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+        }
+    }
+
+    public class KafkaProducerHostedService : IHostedService
+    {
+        private readonly ILogger<KafkaProducerHostedService> _logger;
+        private IProducer<Null, string> _producer;
+
+        public KafkaProducerHostedService(ILogger<KafkaProducerHostedService> logger)
+        {
+            _logger = logger;
+            var config = new ProducerConfig()
+            {
+                BootstrapServers = "localhost:9092"
+            };
+            _producer = new ProducerBuilder<Null, string>(config).Build();
+        }
+        public async Task StartAsync(CancellationToken cancellationToken)
+        {
+            for (var i = 0; i < 50; i++)
+            {
+                var value = $"Hello Kafka {i}";
+                _logger.LogInformation(value);
+                await _producer.ProduceAsync("demo", new Message<Null, string>
+                {
+                    Value = value
+                }, cancellationToken);
+            }
+
+            _producer.Flush(TimeSpan.FromSeconds(10));
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+
         }
     }
 }
